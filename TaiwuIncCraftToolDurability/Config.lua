d1cd36248be8a3b90896c5c4db29ed910b367e8d
yalikes <algebnaly@qq.com>
2024-05-03 21:14:32 +0800

    init commit

---
+++ TaiwuIncCraftToolDurability/Config.lua
@@ -0,0 +1,30 @@
+return {
+	Title = "提升制造工具的最大耐久",
+	Author = "在下炮灰",
+	Cover = "Cover.png",
+	WorkshopCover = "Cover.png",
+	Source = 0,
+	TagList = {
+		[1] = "Modifications",
+	},
+	FrontendPlugins = {
+		[1] = "TaiwuIncCraftToolDurabilityFrontend.dll",
+	},
+	BackendPlugins = {
+		[1] = "TaiwuIncCraftToolDurabilityBackend.dll",
+	},
+	Description = "种田流需要大量做菜和制药，在徒手制造被削弱的情况下, 提升制造工具的最大耐久就显得十分重要了",
+	Version = "1.0.0.0",
+	GameVersion = "0.0.70.57-test",
+	Visibility = 0,
+	DefaultSettings = { },
+	ChangeConfig = false,
+	HasArchive = false,
+	NeedRestartWhenSettingChanged = false,
+	FileId = 3231833733,
+	UpdateLogList = {
+		[1] = {
+			Timestamp = 1714050405,
+		},
+	},
+}
---
+++ TaiwuIncCraftToolDurability/TaiwuIncCraftToolDurabilityBackend/BackendPatch.cs
@@ -0,0 +1,71 @@
+﻿using TaiwuModdingLib.Core.Plugin;
+using GameData.Utilities;
+using GameData.Domains.Building;
+using HarmonyLib;
+using System.Runtime.InteropServices;
+using System.Reflection.Metadata;
+using Config;
+using System.Collections.Generic;
+using TaiwuModdingLib.Core.Utils;
+
+namespace TaiwuIncCraftToolDurabilityBackend
+{
+    [PluginConfig("TaiwuIncCraftToolDurability", "在下炮灰", "0.1.0")]
+    public class BackendPatch: TaiwuRemakePlugin
+    {
+        Harmony harmony;
+        public override void Dispose()
+        {
+            if(harmony != null)
+            {
+                harmony.UnpatchSelf();
+            }
+        }
+
+        public override void Initialize()
+        {
+            harmony = new Harmony("com.paohui.mod");
+            harmony.PatchAll();
+
+            AdaptableLog.Info("提升制造工具的最大耐久 后端Mod 启动了");
+        }
+    }
+
+    [HarmonyPatch(typeof(CraftTool), "GetItem")]
+    public class PatchGalbalConfigBackend
+    {
+        static bool is_modified = false;
+        public static void Prefix()
+        {
+            if (!is_modified)
+            {
+                try
+                {
+                    if(CraftTool.Instance == null)
+                    {
+                        return;
+                    }
+                    List<CraftToolItem>  _dataArray = (List<CraftToolItem>)typeof(CraftTool).GetField("_dataArray", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(CraftTool.Instance);
+                    if(_dataArray == null)
+                    {
+                        return;
+                    }
+                    if(_dataArray.Count >= 54)
+                    {
+                        for(int i = 0; i < 54; i++)
+                        {
+                            if((i % 9) < 5)
+                            {
+                                _dataArray[i].ModifyField("MaxDurability", (short)(3 * _dataArray[i].MaxDurability));
+                            }
+                        }
+                    }
+                }catch(System.Reflection.TargetInvocationException)
+                {
+                    AdaptableLog.Info("exception in reflection 制造工具耐久MOD");
+                }
+                is_modified = true;
+            }
+        }
+    }
+}
