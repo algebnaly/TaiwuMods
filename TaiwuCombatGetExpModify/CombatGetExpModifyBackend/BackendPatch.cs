88ae2e8d740545ec27f217d3d4eccea915858d22
algebnaly <mail@algebnaly.com>
2025-06-08 11:40:45 +0800

    now it can change area spiritual aquire multipler

--- TaiwuCombatGetExpModify/CombatGetExpModifyBackend/BackendPatch.cs
+++ TaiwuCombatGetExpModify/CombatGetExpModifyBackend/BackendPatch.cs
@@ -1,4 +1,4 @@
-﻿using TaiwuModdingLib.Core.Plugin;
+using TaiwuModdingLib.Core.Plugin;
 using GameData.Utilities;
 using GameData.Domains.Item;
 using HarmonyLib;
@@ -9,19 +9,21 @@ using System.Diagnostics;
 using GameData.Domains.Combat;
 using GameData.Domains;
 using GameData.Domains.Mod;
+using GameData;
 
 
 namespace CombatGetExpModify
 {
-    [PluginConfig("CombatGetExpModify", "在下炮灰", "0.1.0")]
-    public class BackendPatch: TaiwuRemakePlugin
+    [PluginConfig("CombatGetExpModify", "algebnaly", "0.1.0")]
+    public class BackendPatch : TaiwuRemakePlugin
     {
-        public static int scale_factor_value = 30;
+        public static int scale_factor_value = 3;
+        public static int scale_area_spiritual_debt_factor_value = 10;
         public static string modIdStr;
         Harmony harmony;
         public override void Dispose()
         {
-            if(harmony != null)
+            if (harmony != null)
             {
                 harmony.UnpatchSelf();
             }
@@ -29,7 +31,7 @@ namespace CombatGetExpModify
 
         public override void Initialize()
         {
-            harmony = new Harmony("com.paohui.mod");
+            harmony = new Harmony("com.algebnaly.mod");
             harmony.PatchAll();
             modIdStr = this.ModIdStr;
         }
@@ -51,14 +53,27 @@ namespace CombatGetExpModify
             var is_success = DomainManager.Mod.GetSetting(BackendPatch.modIdStr, "combat_get_exp_scale_factor", ref BackendPatch.scale_factor_value);
             if (!is_success)
             {
-                BackendPatch.scale_factor_value= 30;
-                AdaptableLog.Info("战斗历练获取修改: 没有能够读取设置" + BackendPatch.scale_factor_value);
+                AdaptableLog.Info("战斗恩义历练获取修改: 没有能够读取设置" + BackendPatch.scale_factor_value);
             }
-            double scale_factor = BackendPatch.scale_factor_value/10.0;
             for (int i = 0; i < GlobalConfig.Instance.CombatGetExpBase.Length; i++)
             {
-                GlobalConfig.Instance.CombatGetExpBase[i]=(short)(orginalCombatGetExpBase[i] * scale_factor);
+                GlobalConfig.Instance.CombatGetExpBase[i] = (short)(orginalCombatGetExpBase[i] * BackendPatch.scale_factor_value);
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(CombatDomain))]
+    [HarmonyPatch("GetAddAreaSpiritualDebt", typeof(int[]))]
+    public class PatchSpiritualAquire
+    {
+        public static void Postfix(int[] enemyList, ref int __result)
+        {
+            var is_success = DomainManager.Mod.GetSetting(BackendPatch.modIdStr, "combat_get_area_spiritual_debt_scale_factor", ref BackendPatch.scale_area_spiritual_debt_factor_value);
+            if (!is_success)
+            {
+                AdaptableLog.Info("战斗恩义历练获取修改: 没有能够读取设置" + BackendPatch.scale_factor_value);
+            }
+            __result = __result * BackendPatch.scale_area_spiritual_debt_factor_value;
         }
     }
 }
--- TaiwuCombatGetExpModify/Config.lua
+++ TaiwuCombatGetExpModify/Config.lua
@@ -1,11 +1,11 @@
 return {
-	Title = "[测试]战斗历练获取修改",
+	Title = "[测试]战斗历练恩义获取修改",
 	Author = "algebnaly",
 	Cover = "Cover.png",
 	WorkshopCover = "Cover.png",
 	Source = 0,
-	GameVersion = "0.0.77.18-test",
-	Version = "1.0.0.2",
+	GameVersion = "0.0.78.24-test",
+	Version = "1.0.1.0",
 	FileId = 3172379059,
 	Visibility = 0,
 	UpdateLogList = {
@@ -24,6 +24,9 @@ return {
 				[1] = "修复了战斗后爆红字的错误",
 			},
 		},
+		[4] = {
+			Timestamp = 1749353981,
+		},
 	},
 	TagList = {
 		[1] = "Modifications",
@@ -31,17 +34,27 @@ return {
 	BackendPlugins = {
 		[1] = "CombatGetExpModifyBackend.dll",
 	},
-	Description = "        简单地增加战斗历练获取基础值\n        原来的历练获取基础值为\n        {\n          15, 30, 60, 90, 135, 180, 240, 315, 390, 480,\n          570, 690, 840, 1020, 1230, 1500, 1830, 2280, 2850\n        };\n        我将其设置为该基础值的倍数, 向下取整。\n        默认为3倍, 可以在mod设置中修改, 注意不要超过11倍否则可能数值溢出倒扣历练值!\n        Github: https://github.com/yalikes/TaiwuCombatGetExpModify\n        ",
+	Description = "        简单地增加战斗历练获取基础值\n        原来的历练获取基础值为\n        {\n          15, 30, 60, 90, 135, 180, 240, 315, 390, 480,\n          570, 690, 840, 1020, 1230, 1500, 1830, 2280, 2850\n        };\n        我将其设置为该基础值的倍数, 向下取整。\n        默认为3倍, 可以在mod设置中修改, 注意不要超过11倍否则可能数值溢出倒扣历练值!\n\t现在可以修改恩义获取倍数了\n        ",
 	DefaultSettings = {
 		[1] = {
 			SettingType = "Slider",
 			Key = "combat_get_exp_scale_factor",
-			DisplayName = "战斗历练获取倍数设置(单位: 0.1倍)",
-			Description = "1个单位表示原获取历练的0.1倍, 默认值20表示获取历练为原来的两倍",
+			DisplayName = "战斗历练获取倍数设置",
+			Description = "战斗历练获取的倍数",
 			MinValue = 1,
-			MaxValue = 110,
+			MaxValue = 100,
 			StepSize = 1,
-			DefaultValue = 30,
+			DefaultValue = 3,
+		},
+		[2] = {
+			SettingType = "Slider",
+			Key = "combat_get_area_spiritual_debt_scale_factor",
+			DisplayName = "战斗地区恩义获取倍数设置",
+			Description = "战斗地区恩义获取的倍数",
+			MinValue = 1,
+			MaxValue = 100,
+			StepSize = 1,
+			DefaultValue = 10,
 		},
 	},
 	ChangeConfig = false,
